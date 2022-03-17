b1a9db9ee370725e132eb953b7aafde511bbf959
Simone Vellei <henomis@gmail.com>
2022-03-17 20:56:15 +0100

    chore: add Rt calculation

--- internal/pkg/render/italia.go
+++ internal/pkg/render/italia.go
@@ -2,6 +2,7 @@ package render
 
 import (
 	"io/ioutil"
+	"math"
 
 	"github.com/henomis/covid-go/internal/pkg/chartjs"
 	"github.com/henomis/covid-go/internal/pkg/data"
@@ -11,15 +12,17 @@ const (
 	datiItaliaEndpoint        = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv"
 	popolazioneItaliaEndpoint = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv"
 
-	italiaDataframeKey              = "italia"
-	popolazioneItaliaDataframeKey   = "italia_popolazione"
-	italiaNuoviPositiviDataframeKey = "italia_nuovi_positivi"
+	italiaDataframeKey               = "italia"
+	popolazioneItaliaDataframeKey    = "italia_popolazione"
+	italiaNuoviPositiviDataframeKey  = "italia_nuovi_positivi"
+	italiaPositiviTotaliDataframeKey = "italia_positivi_totali"
 )
 
 func (r *Render) Italia() {
 
 	r.importBasicData()
 	r.nuoviPositivi()
+	r.RT()
 
 }
 
@@ -77,4 +80,62 @@ func (r *Render) nuoviPositivi() {
 	r.dataset.Delete(italiaNuoviPositiviDataframeKey)
 
 	ioutil.WriteFile(r.outputPath+"italia_nuovi_positivi.json", []byte(value), 0666)
+}
+
+func (r *Render) RT() {
+
+	r.dataset.SelectAndCopy(italiaDataframeKey, italiaPositiviTotaliDataframeKey, []string{"data", "totale_positivi"})
+
+	calculateRT := func(records []float64) []float64 {
+
+		newRecords := make([]float64, len(records))
+
+		for i := range records {
+
+			if i == 0 {
+				newRecords[0] = 0.0
+				continue
+			}
+
+			Pn := records[i]
+			Pn1 := records[i-1]
+
+			En := (Pn - Pn1) / Pn1
+
+			Rt := math.Exp(5 * En)
+			if math.IsInf(Rt, 0) || math.IsNaN(Rt) {
+				Rt = 1
+			}
+
+			newRecords[i] = Rt
+
+		}
+
+		return newRecords
+
+	}
+
+	r.dataset.ColCalculateFloat(italiaPositiviTotaliDataframeKey, []string{"totale_positivi"}, calculateRT)
+
+	avg7 := r.dataset.NewAVG7Dataset(italiaPositiviTotaliDataframeKey, "totale_positivi")
+
+	chart1 := chartjs.New()
+	value := chart1.NewScatteredLineGraph(
+		r.dataset.DatasetAsStrings(italiaPositiviTotaliDataframeKey, "data"),
+		&chartjs.LineOptions{
+			Label:   "RT AVG7",
+			Color:   "#ff0000",
+			Dataset: avg7,
+		},
+		&chartjs.ScatterOptions{
+			Label:   "RT",
+			Color:   "rgba(196,196,196,0.44)",
+			Dataset: r.dataset.DatasetAsFloats(italiaPositiviTotaliDataframeKey, "totale_positivi"),
+		},
+	)
+
+	r.dataset.Delete(italiaPositiviTotaliDataframeKey)
+
+	ioutil.WriteFile(r.outputPath+"italia_rt.json", []byte(value), 0666)
+
 }
